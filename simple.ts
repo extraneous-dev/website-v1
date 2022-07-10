4afa4f2d0c6d6d8eaeca0a1508d8fe5ff3b95c98
Jose Sanchez <josersanchez0117@gmail.com>
2022-07-10 14:10:26 -0500

    small edits, analytics setup, etc

--- simple.ts
+++ simple.ts
@@ -2,20 +2,20 @@ const canvas = <HTMLCanvasElement> document.getElementById('animCanvas');
 const context = canvas.getContext('2d');
 
 const sections: any = document.querySelectorAll('section').keys();
-console.log(sections)
+// console.log(sections)
 
-const intersectionOptions = {
-    threshold: .75
-}
+// const intersectionOptions = {
+//     threshold: .75
+// }
 
-const callback = (entries: IntersectionObserverEntry[]) => {
-    const item = entries[0];
+// const callback = (entries: IntersectionObserverEntry[]) => {
+//     const item = entries[0];
 
-    if (item.isIntersecting) {
-        console.log("AAAA");
-    } 
-}
+//     if (item.isIntersecting) {
+//         console.log("AAAA");
+//     } 
+// }
 
-const observer = new IntersectionObserver(callback, intersectionOptions);
+// const observer = new IntersectionObserver(callback, intersectionOptions);
 
-sections.map(val => observer.observe(val));
+// sections.map(val => observer.observe(val));
